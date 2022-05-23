2fce43296a14d339da301b5a2573362e4ccfad80
balamanikandan9232 <93114754+balamanikandan9232@users.noreply.github.com>
2022-05-23 10:43:31 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,18 @@
+// const numbers = (n)=>{
+//     let sum = 0;
+//     for(var i=1;i<=n;i++){
+//         sum +=i
+//     }
+// }
+// [2,3,5,7]
+
+// var twosum = function(nums,target){
+//     for(let i=0;i<nums.length;i++){
+//         for(let j =i+1; j<nums.length; j++){
+//             if(nums[i]+nums[j]===target){
+//                 return[i,j]
+//             }
+//         }
+//     }return[-1,-1]
+// }
+
